7e11ca92cd057e07fd8db9a132cdfe8ad7d57e47
David Recanati <davidd.r.90@gmail.com>
2018-04-20 13:21:46 +0300

    add ActionSheet enable gallery and camera options

--- src/pages/add-phrase/add-phrase.ts
+++ src/pages/add-phrase/add-phrase.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, ActionSheetController, ViewController } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 
 
 //look in this link for more info abuot images
 //https://blog.ionicframework.com/ionic-native-accessing-ios-photos-and-android-gallery-part-i/
+
+enum ImageOptions {
+  CAMERA = 1,
+  GALLERY,
+  SEARCH
+}
 
 @IonicPage()
 @Component({
@@ -14,9 +20,20 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 })
 export class AddPhrasePage {
 
+  @ViewChild('fileInput') fileInput;
+
+
+
+
+
   private _myForm: FormGroup;
 
-  constructor(private _formBuilder: FormBuilder, private _camera: Camera) {
+  constructor(private _formBuilder: FormBuilder,
+    private _camera: Camera,
+    private _actionSheetCtrl: ActionSheetController,
+    private _viewCtrl: ViewController) {
+
+
     this._myForm = this._formBuilder.group({
       //this should be an object like the phrase object
       "text": ['', Validators.required],//the phrase
@@ -27,29 +44,114 @@ export class AddPhrasePage {
   }
 
   ionViewDidLoad() {
-    
+
   }
 
-  onSubmit(){
+  onSubmit() {
     console.log(this._myForm.value);
     // use the form object to create new phares object and add it to the server
+    if (!this._myForm.valid) { return; }
+    this._viewCtrl.dismiss(this._myForm.value);//return the nw object
   }
 
+  //present Action Sheet when press the add button
+  //let the user choose from where to get the image
+  //the user have three options:
+  //  1. from the build in camera
+  //  2. from the device picture gallery
+  //  3. from google with an online seaech
+  presentActionSheet() {
+    let actionSheet = this._actionSheetCtrl.create({
+      title: 'בחר מקור לתמונה',
+      buttons: [
+        {
+          text: 'מצלמה',
+          icon: 'camera',
+          handler: () => {
+            console.log('camera');
+            this.getPicture(ImageOptions.CAMERA);
+          }
+        },
+        {
+          text: 'גלריה',
+          icon: 'images',
+          handler: () => {
+            console.log('gallery');
+            this.getPicture(ImageOptions.GALLERY);
+          }
+        },
+        {
+          text: 'חיפוש ברשת',
+          icon: 'logo-google',
+          handler: () => {
+            console.log('search on line');
+            this.getPicture(ImageOptions.SEARCH);
+          }
+        },
+
+        {
+          text: 'ביטול',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+
+    actionSheet.present();
+  }
+
+
   //check all the next functions
-  getPicture() {
-    if (Camera['installed']()) {//if there is a camera install in this device
-      this._camera.getPicture({
-        destinationType: this._camera.DestinationType.DATA_URL,
-        targetWidth: 96,
-        targetHeight: 96
-      }).then((data) => {
-        this._myForm.patchValue({ 'imagePath': 'data:image/jpg;base64,' + data });//insert the capture image path to the form 
-      }, (err) => {
-        alert('Unable to take photo');
-      })
-    } else {//if there is NOT camera on this device go to the file maneger to look for an image
-      // this.fileInput.nativeElement.click();
+  getPicture(from: any) {
+    let cameraOptions;
+
+    switch (from) {
+      case ImageOptions.CAMERA:
+        if (Camera['installed']()) {//if there is a camera install in this device
+          cameraOptions = {
+            sourceType: this._camera.PictureSourceType.CAMERA,
+            destinationType: this._camera.DestinationType.DATA_URL,
+            targetWidth: 96,
+            targetHeight: 96,
+            encodingType: this._camera.EncodingType.JPEG,
+            correctOrientation: true
+          }
+          break;
+        }
+        //if there is NOT a camera on this device
+      case ImageOptions.GALLERY:
+
+        // cameraOptions = {
+        //   sourceType: this._camera.PictureSourceType.PHOTOLIBRARY,
+        //   destinationType: this._camera.DestinationType.DATA_URL,
+        //   quality: 100,
+        //   targetWidth: 1000,
+        //   targetHeight: 1000,
+        //   encodingType: this._camera.EncodingType.JPEG,
+        //   correctOrientation: true
+        // }
+
+        //if there is NOT camera on this device go to the file maneger to look for an image
+        this.fileInput.nativeElement.click();
+        break;
+
+      case ImageOptions.SEARCH:
+        //go to google search with the input text if there is...
+        break;
+      default:
+        break;
     }
+
+
+    this._camera.getPicture(cameraOptions).then((data) => {
+      this._myForm.patchValue({ 'imagePath': 'data:image/jpg;base64,' + data });//insert the capture image path to the form 
+    }, (err) => {
+      console.log(err);
+      alert('Unable to take photo');
+    })
+
   }
 
   processWebImage(event) {
@@ -67,6 +169,12 @@ export class AddPhrasePage {
     return 'url(' + this._myForm.controls['imagePath'].value + ')'
   }
 
-  
+  /**
+   * The user cancelled, so we dismiss without sending data back.
+   */
+  cancel() {
+    this._viewCtrl.dismiss();
+  }
+
 
 }
