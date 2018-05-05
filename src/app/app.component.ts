b8a7113f2242f86022341bb002f905213f16ad41
David Recanati <davidd.r.90@gmail.com>
2018-05-05 22:36:53 +0300

    reset the app component to the home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { NothingPage } from '../pages/nothing/nothing';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = AddPhrasePage;
+  rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
