37e23e83840dd531c1dd22d0e90f4fe7257c6a9e
David Recanati <davidd.r.90@gmail.com>
2018-05-10 09:10:52 +0300

    connect the categoryPage to the Add page return an object from the form need to upload the return object to the DB

--- src/pages/categories/categories.ts
+++ src/pages/categories/categories.ts
@@ -1,18 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 import { CategoryServiceProvider } from '../../providers/category-service/category-service';
 import { Client } from '../../models/Client';
-import { HomePage } from '../home/home';
-import { AboutMePage } from '../about-me/about-me';
 import { Category } from '../../models/Category';
+import { AddPhrasePage } from '../add-phrase/add-phrase';
 
 
-/**
- * Generated class for the CategoriesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+const isCategory = true;
+const CATEGORY = 1;
 
 @IonicPage()
 @Component({
@@ -23,13 +18,30 @@ export class CategoriesPage {
 
   private client: Client;
 
-  constructor(public categoryService: CategoryServiceProvider,public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public categoryService: CategoryServiceProvider,
+    public navParams: NavParams,
+    public modalCtrl:ModalController) {
     
   }
 
 
-
   ionViewDidLoad() {
+  }
+
+  /**display the addPhrasePage and get the retrun object from the form.
+   * Prompt the user to add a new category. This shows our AddPhrasePage in a
+   * modal and then adds the new item to our data source if the user created one.
+   */
+  openAddPage() {
+    let addModal = this.modalCtrl.create('AddPhrasePage',  {'fromWhere': CATEGORY});
+    addModal.onDidDismiss(item => {
+      if (item) {//if there is an object that return from the form
+        console.log(item);
+        //TOOD: here we should upload the 'item' to the DB using Or & Dor firebaseProvider
+      }
+    })
+    addModal.present();//present the addPhrasePage
   }
 
 
