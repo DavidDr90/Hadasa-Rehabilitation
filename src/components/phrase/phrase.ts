9c96ed758ea9351713709d230df552393b0d7e0b
Alex Shimanovich <volkov1988@gmail.com>
2018-05-20 18:54:54 +0300

    added content to phrase popup page, still no sound

--- src/components/phrase/phrase.ts
+++ src/components/phrase/phrase.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Phrase } from "../../models/Phrase"
 import { PhrasePopupPage } from '../../pages/phrase-popup/phrase-popup';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'phrase',
@@ -12,11 +12,30 @@ export class PhraseComponent {
 
   @Input() phrase : Phrase;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
   }
 
+  /*open the phrase popup page and pass to it
+   current phrase , image, audio*/
   public openPopup(){
-    this.navCtrl.push(PhrasePopupPage);
+    try{
+      this.navCtrl.push(PhrasePopupPage, {
+        phraseName: this.phrase.GetName(),
+        phraseImageURL: this.phrase.GetImageURL(), 
+        //TODO add audio to phrase model and pass it here 
+        //phraseAudioURL: "this.phrase.GetAudioURL"     
+      });
+    }
+    catch{
+
+        let alert = this.alertCtrl.create({
+          title: 'cannot open phrase popup',
+          buttons: ['sorry']
+        });
+        alert.present();
+
+    }
+
 }
 
 }
--- src/pages/phrase-popup/phrase-popup.ts
+++ src/pages/phrase-popup/phrase-popup.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the PhrasePopupPage page.
@@ -14,9 +14,18 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'phrase-popup.html',
 })
 export class PhrasePopupPage {
+  private phraseName: string;
+  private phraseImageURL: string;
+  private phraseAudioURL: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController){
+    this.phraseName = this.navParams.get("phraseName");
+    this.phraseImageURL = this.navParams.get("phraseImageURL");
+
+    //TODO use this when audio is implemented in phrase model
+    //this.phraseAudioURL = this.navParams.get("phraseAudioURL");
+  } 
+  
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PhrasePopupPage');
@@ -24,7 +33,12 @@ export class PhrasePopupPage {
 
   //go back to the page that called this popup
   private back(){
-    this.navCtrl.pop(); 
+    let alert = this.alertCtrl.create({
+      title: this.navParams.get("phraseName"),
+      buttons: ['fuck']
+    });
+    alert.present();
+    //this.navCtrl.pop(); 
   }
 
 }
