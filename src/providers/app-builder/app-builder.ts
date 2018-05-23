a609154fc2ee705a4fb98a4e71372e3076c41301
ofekba <ofek1b@gmail.com>
2018-05-23 09:48:40 +0300

    added the method for the app-builder component that add default categories&phrases to the user DB when he sign up to the app for the first time

--- src/providers/app-builder/app-builder.ts
+++ src/providers/app-builder/app-builder.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Phrase } from '../../models/Phrase'
+import { Category } from '../../models/Category'
+import { User } from '../../models/user'
+import { PhrasesProvider } from '../phrases/phrases'
+import { CategoryServiceProvider } from '../category-service/category-service'
 
 /**
  * this provider create the defult categorys and phrases when the app is first load
@@ -8,7 +13,29 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AppBuilderProvider {
 
-  constructor() {
+  constructor(public categoryProvider:CategoryServiceProvider, public phraseProvider:PhrasesProvider, public user:User) {
   }
+
+   //===================================
+  /**this method create and add new phrase to the user DB.
+   * @param name, the name of the phrase
+   * @param imageURL, the imageURL of the phrase's image
+   * @param categoryID, the ID of the category which the phrase belong to
+   * @param audio, the URL of the audio file of the phrase.
+   */
+  createDefPhrase(name:string, imageURL:string, categoryID:string, audio:string){
+   this.phraseProvider.AddNewPhrase("", name, imageURL, categoryID, 0, audio, false);
+  }
+
+   //===================================
+  /**this method create and add new category to the user DB.
+   * @param name, the name of the category
+   * @param imageURL, the imageURL of the category's image
+   * @param parentCategoryID, the ID of the category which the category belong to
+   */
+  createDefCat(name:string, imageURL:string, parentCategoryID:string){
+    let cat=new Category(name, "", imageURL, this.user.getEmail, parentCategoryID, 0, false);
+    this.categoryProvider.addCategory(cat);
+   }
 
 }
