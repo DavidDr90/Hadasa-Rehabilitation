84a85843777e891cf993395f6b6d01035bd14c5b
David Recanati <davidd.r.90@gmail.com>
2018-05-23 09:27:10 +0300

    add appBuilder provider

---
+++ src/providers/app-builder/app-builder.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+/**
+ * this provider create the defult categorys and phrases when the app is first load
+ */
+
+
+@Injectable()
+export class AppBuilderProvider {
+
+  constructor() {
+  }
+
+}
