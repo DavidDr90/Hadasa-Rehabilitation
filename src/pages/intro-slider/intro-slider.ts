504442ea92697f76c037dc7e7386a8d4cbefc66a
David Recanati <davidd.r.90@gmail.com>
2018-05-23 12:40:30 +0300

    fix intro slides UI

--- src/pages/intro-slider/intro-slider.ts
+++ src/pages/intro-slider/intro-slider.ts
@@ -1,4 +1,4 @@
-import { Component,ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams, Slides, LoadingController } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs'
 import { AutenticationProvider } from '../../providers/autentication/autentication';
@@ -12,35 +12,31 @@ import * as firebase from 'firebase/app';
   selector: 'page-intro-slider',
   templateUrl: 'intro-slider.html'
 })
-export class IntroSliderPage 
-{
+export class IntroSliderPage {
   tabsPage = TabsPage;
   appPage = MyApp
   loading_sign = false;
-  
-  //allows to access its childeren (individual slides)
-  @ViewChild(Slides) slides:Slides; 
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public authentication: AutenticationProvider) {
-   // this.checkIfDataLoaded();
-   this.loading_sign = false;
+    // this.checkIfDataLoaded();
+    this.loading_sign = false;
   }
 
   ionViewDidLoad() {
     //On page loading checks if the user already logged in.
     //if the user logged in already sets 'TabsPage' as main page.
-    this.authentication.afAuth.auth.onAuthStateChanged((user)=>{
-      if(user)
+    this.authentication.afAuth.auth.onAuthStateChanged((user) => {
+      if (user)
         this.navCtrl.setRoot(TabsPage);
     })
   }
-  
+
   // This function called by clicking login-button.
-  public async logIn()
-  {
+  public async logIn() {
     this.loading_sign = true;
     // The function toggleSignIn will connect the user with redirect-auth.
     let promise = this.authentication.toggleSignIn();
-  } 
+  }
 }
-  
+
