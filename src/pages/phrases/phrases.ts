26f2237a1703184fbea4537f4e8ee56a3e0de697
David Recanati <davidd.r.90@gmail.com>
2018-05-23 10:36:37 +0300

    upload new phrase and new category to the DB in phrase page can choose to add new phrase or new sub category

--- src/pages/phrases/phrases.ts
+++ src/pages/phrases/phrases.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController, ActionSheetController } from 'ionic-angular';
 import { PhrasesProvider } from '../../providers/phrases/phrases';
 import { Category } from '../../models/Category';
 import { Phrase } from '../../models/Phrase';
 
 import * as Enums from '../../consts/enums';
+import { CategoryServiceProvider } from '../../providers/category-service/category-service';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 
 @IonicPage()
@@ -16,78 +18,120 @@ export class PhrasesPage {
 
   public parentCategory: Category;
   public phrases;
-  
-  constructor(public navCtrl: NavController, 
-              public navParams: NavParams,
-              public phrasesProvider: PhrasesProvider,
-              public modalCtrl:ModalController,) 
-  {
+
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public phrasesProvider: PhrasesProvider,
+    public modalCtrl: ModalController,
+    private _actionSheetCtrl: ActionSheetController,
+    public categoryService: CategoryServiceProvider,
+    public aAuth: AngularFireAuth,
+
+  ) {
     //get the parent category object from the clickable category.
     this.parentCategory = navParams.get('parentCategory');
     console.log("perent is = " + this.parentCategory);
     this.AsyncPhrasesloader();
   }
-  
+
 
   //handler that add phrase and update the display 
-  public addPhrase(phrase: Phrase)
-  {
-    console.log("in add");
-    console.log(phrase);
-    setTimeout(()=>{
+  public addPhrase(phrase: Phrase) {
+    setTimeout(() => {
       this.phrasesProvider.addPhrase(phrase);
       this.AsyncPhrasesloader()
     }, 500)
-  }  
+  }
 
-    //handler that remove phrase and update the display 
-    public removePhrase(phrase: Phrase)
-    {
-      setTimeout(()=>{
-        this.phrasesProvider.removePhrase(phrase);
-        this.AsyncPhrasesloader()
-      }, 500)
-    }  
-  
+  //handler that remove phrase and update the display 
+  public removePhrase(phrase: Phrase) {
+    setTimeout(() => {
+      this.phrasesProvider.removePhrase(phrase);
+      this.AsyncPhrasesloader()
+    }, 500)
+  }
+
   //initial phrases array for ngFor
   //promise is an Promise object that gets the return value only when its ready (await)
   // from phrase provider.
   //temp is an promise object that help to get the phrases from promis's resolve attr.
-  public async AsyncPhrasesloader(){
+  public async AsyncPhrasesloader() {
     let promise = await this.phrasesProvider.getPhrases(this.parentCategory);
-    let temp = new Promise ((resolve,reject) => {
+    let temp = new Promise((resolve, reject) => {
       resolve(promise);
     });
-    temp.then((data)=>{
+    temp.then((data) => {
       this.phrases = data;
     })
   }
 
+
+  /**present Action Sheet when press the add button
+   * let the user choose what he wants to add
+   * the user have three options:
+   * 1. add new sub-category
+   * 2. add new phrase
+   */
+  presentActionSheet() {
+    let actionSheet = this._actionSheetCtrl.create({
+      title: 'בחר מה להוסיף',
+      buttons: [
+        {
+          text: '\xa0\xa0 הוספת ביטוי',
+          icon: 'camera',
+          handler: () => {
+            this.openAddPage(Enums.ADD_OPTIONS.PHRASE);
+          }
+        },
+        {
+          text: '\xa0\xa0 הוספת תת-קטגוריה',
+          icon: 'images',
+          handler: () => {
+            this.openAddPage(Enums.ADD_OPTIONS.CATEGORY);
+          }
+        },
+        {
+          text: 'ביטול',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+
+    actionSheet.present();
+  }
 
 
   /**display the addPhrasePage and get the retrun object from the form.
   * Prompt the user to add a new phrase. This shows our AddPhrasePage in a
   * modal and then adds the new item to our data source if the user created one.
   */
-  openAddPage() {
+  openAddPage(fromWhere) {
     let addModal = this.modalCtrl.create('AddPhrasePage',
-    { 'fromWhere': Enums.ADD_OPTIONS.PHRASE, 'categoryName': this.parentCategory.name });
+      { 'fromWhere': fromWhere, 'categoryName': this.parentCategory.id });
     addModal.onDidDismiss(item => {
       if (item) {//if there is an object that return from the form
-        console.log(item);
-        //TOOD: here we should upload the 'item' to the DB using Or & Dor firebaseProvider
-        let newPhrase = 
-        new Phrase("", item.text, item.imagePath, item.categoryID, 0, item.audioFile, false);
-        console.log("new phrase is:");
-        console.log(newPhrase);
-        this.addPhrase(newPhrase);
+
+        if (fromWhere == Enums.ADD_OPTIONS.PHRASE) {
+          //create new phrase from the output form
+          let newPhrase =
+            new Phrase("", item.text, item.imagePath, item.categoryID, 0, item.audioFile, false);
+          this.addPhrase(newPhrase);//upload the new phase to the DB and refresh the screen
+        } else if (fromWhere == Enums.ADD_OPTIONS.CATEGORY) {
+          let newSubCategory =
+          new Category(item.text, "", item.imagePath, this.aAuth.auth.currentUser.email, item.categoryID, 0, false);
+          this.categoryService.addCategory(newSubCategory);
+
+        }
       }
     })
     addModal.present();//present the addPhrasePage
   }
 
   //TODO: enter edit mode
-  edit(){
+  edit() {
     console.log("edit phrase");
   }
 
