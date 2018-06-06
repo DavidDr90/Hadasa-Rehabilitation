5441ec8ba57bdedd80f9e13c6504e58a0ffa9e8b
David Recanati <davidd.r.90@gmail.com>
2018-06-06 21:59:48 +0300

    add some improvments to pop up

--- src/pages/phrase-popup/phrase-popup.ts
+++ src/pages/phrase-popup/phrase-popup.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
 //for the recorder functions
 import { Media, MediaObject } from '@ionic-native/media';
 import { ErrorProvider } from '../../providers/error/error';
@@ -21,19 +21,39 @@ export class PhrasePopupPage {
   private phraseAudioURL: string;
   private audio: MediaObject;
   private windowClosed: boolean = false;
+  private isIOS: boolean = false;
+  private audioUrlIsEmpty: boolean = false;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private errorProvider: ErrorProvider,
     /* media providers for playing audio */
-    private media: Media, ) {
+    private media: Media,
+    private platform: Platform) {
+
+    //if the platform is iOS display the nav bar
+    if (platform.is('ios'))
+      this.isIOS = true;
+    else
+      this.isIOS = false;
 
     this.phraseName = this.navParams.get("phraseName");
     this.phraseImageURL = this.navParams.get("phraseImageURL");
     this.phraseAudioURL = this.navParams.get("phraseAudioURL");
 
     this.playAudio(this.phraseAudioURL);
+
+    /**handle the minimize events
+     * pause when the app is minimize
+     * replay when the app is up agian
+     */
+    document.addEventListener("pause", () => {
+      this.audio.pause();
+    }, false);
+    document.addEventListener("resume", () => {
+      this.audio.play();
+    }, false);
   }
 
   /** play the input file on the device speakers
@@ -42,28 +62,32 @@ export class PhrasePopupPage {
   private playAudio(url) {
 
     //if we have no audio, whait few sec and pop
-    if (url == null || url == "") {
+    if (url == null || url == "" || url == undefined) {
+      this.audioUrlIsEmpty = true;
       setTimeout(
         this.navCtrl.pop(),
-        3000
+        60000//display for a decent time, one minute for now
       )
     }
 
-    this.audio = this.media.create(url);
+    try {
+      this.audio = this.media.create(url);
 
-    this.audio.onStatusUpdate.subscribe(status => {
-      if (status.toString() == "1") { //player start
-        console.log("start playing");
-      }
-      if (status.toString() == "4") { // player end running
-        console.log("player stopped");
-        if (!this.windowClosed) {
-          console.log("in closed if");
-          this.audio.release();//free audio resources after playback (android)
-          this.navCtrl.pop();
+      this.audio.onStatusUpdate.subscribe(status => {
+        if (status.toString() == "1") { //player start
+          console.log("start playing");
         }
-      }
-    });
+        if (status.toString() == "4") { // player end running
+          console.log("player stopped");
+          if (!this.windowClosed) {
+            this.audio.release();//free audio resources after playback (android)
+            this.navCtrl.pop();
+          }
+        }
+      });
+    } catch (err) {
+      this.errorProvider.simpleTosat(err);
+    }
 
     try {
       this.audio.play()
@@ -78,8 +102,10 @@ export class PhrasePopupPage {
   //make sure to close the audio when the page is closed
   ionViewDidLeave() {
     this.windowClosed = true;
-    this.audio.pause();
-    this.audio.release();
+    if (!this.audioUrlIsEmpty) {
+      this.audio.pause();
+      this.audio.release();
+    }
   }
 
 }
