675b3180e5fdf8f64b767eb017c07a2cde89e1e2
ofekba <ofek1b@gmail.com>
2018-06-06 10:03:06 +0300

    fixed the on click, the design

--- src/components/my-category/my-category.ts
+++ src/components/my-category/my-category.ts
@@ -44,8 +44,7 @@ export class MyCategoryComponent {
    * @param cat the category that clicked
   */
  public catOnClick(event){
-  event.stopPropagation();
-  // this.category as Category;
+  this.category as Category;
   this.category.views++;// update the category views field
   this.categoryProvider.increaseViews(this.category)// update the category views field in the DB
   this.favProvider.addCommonFavCat(this.category); //check if the category is common category
