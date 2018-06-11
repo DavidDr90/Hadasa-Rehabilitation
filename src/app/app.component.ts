0f3a5e5385773704e37da732ecb721b1e6d0e881
Or Cohen <or.2511.cohen@gmail.com>
2018-06-11 22:37:25 +0300

    Initial paging navigation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform, NavController } from 'ionic-angular';
+import { Platform, NavController, LoadingController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { IntroSliderPage } from '../pages/intro-slider/intro-slider';
@@ -8,6 +8,7 @@ import { AutenticationProvider } from '../providers/autentication/autentication'
 import { CategoryServiceProvider } from '../providers/category-service/category-service';
 import { Category } from '../models/Category';
 import { AboutMeFormPage } from '../pages/about-me-form/about-me-form';
+import * as Enums from '../consts/enums';
 
 const ABOUT_ME_STRING = "aboutMe";
 
@@ -20,32 +21,48 @@ export class MyApp {
   aboutMeCategory: Category;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
-    public authentication: AutenticationProvider) {
+    public authentication: AutenticationProvider,private categoryServiceProvider: CategoryServiceProvider,private loadingCtrl: LoadingController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
-
     });
 
     //check if current user is login, initialize authentication.loggedIn attr.
     // this.authentication.isLoggedIn();
     this.authentication.initApp();
+
     //considering authentication.loggedIn set the following page: homePage or loginPage
     this.getRootPage();
+   
   }
 
   //---------------------------------------------------------------------------//
   //considering authentication.loggedIn set the following page: homePage or loginPage
   //if current user doesn't logged in -> popup google login window.  
   getRootPage() {
-    if (this.authentication.isLoggedIn()) {
-      this.rootPage = TabsPage;
+    this.authentication.logOut()
+    this.authentication.afAuth.auth.onAuthStateChanged(
+    async user =>
+    {
+     if(user)
+      {
+      let update_promise = await this.categoryServiceProvider.updateCategoriesArray()
+      let promise = this.categoryServiceProvider.getCategoryByName(Enums.ABOUT_ME_STRING);//try to get the about me category from the DB
+      promise.then((data) => {
+          console.log("Exists");
+          this.rootPage = TabsPage;
+      },
+        (data) =>
+      {
+        this.rootPage = AboutMeFormPage;
+        console.log("Nothing");
+      })
     }
-    else {
+    })
       this.rootPage = IntroSliderPage;
-    }
+  
   }
   //---------------------------------------------------------------------------//
 
