e932bc592551936738ec0f43b2d973d382e82bc1
David Recanati <davidd.r.90@gmail.com>
2018-06-14 10:12:19 +0300

    move initApp before spalsh hide

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,16 +21,17 @@ export class MyApp {
   aboutMeCategory: Category;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
-    public authentication: AutenticationProvider,private categoryServiceProvider: CategoryServiceProvider,private loadingCtrl: LoadingController) {
+    public authentication: AutenticationProvider, private categoryServiceProvider: CategoryServiceProvider, private loadingCtrl: LoadingController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
+      this.authentication.initApp();
+      this.getRootPage();
       splashScreen.hide();
     });
-    this.authentication.initApp();
-    this.getRootPage();
-   
+
+
   }
 
   //---------------------------------------------------------------------------//
@@ -46,33 +47,29 @@ export class MyApp {
   public getRootPage() {
 
     this.authentication.afAuth.auth.onAuthStateChanged(
-    async user =>
-    {
-      if (user && user.emailVerified)
-      {
-        let loading = this.loadingCtrl.create({
-          content: 'אנא המתן'
-        });
-        loading.present();
-    
-      let update_promise = await this.categoryServiceProvider.updateCategoriesArray()
-      let promise = this.categoryServiceProvider.getCategoryByName(Enums.ABOUT_ME_STRING);//try to get the about me category from the DB
-      promise.then((data) => {
-          console.log("Exists");
-          this.rootPage = TabsPage;
-      },
-        (data) =>
-      {
-        this.rootPage = AboutMeFormPage;
-        console.log("Nothing");
+      async user => {
+        if (user && user.emailVerified) {
+          let loading = this.loadingCtrl.create({
+            content: 'אנא המתן'
+          });
+          loading.present();
+
+          let update_promise = await this.categoryServiceProvider.updateCategoriesArray()
+          let promise = this.categoryServiceProvider.getCategoryByName(Enums.ABOUT_ME_STRING);//try to get the about me category from the DB
+          promise.then((data) => {
+            console.log("Exists");
+            this.rootPage = TabsPage;
+          },
+            (data) => {
+              this.rootPage = AboutMeFormPage;
+              console.log("Nothing");
+            })
+          loading.dismiss()
+        }
+        else {
+          this.rootPage = IntroSliderPage;
+        }
       })
-      loading.dismiss()
-      }
-      else
-      {
-        this.rootPage = IntroSliderPage;
-      }
-    })
   }
   public setRootPage(page) {
     this.rootPage = page
