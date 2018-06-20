97912956e75b61b49d1a9149df8e27579088688e
David Recanati <davidd.r.90@gmail.com>
2018-06-20 22:07:51 +0300

    disable sticky in favorite html on ios

--- src/components/favorites/favorites.ts
+++ src/components/favorites/favorites.ts
@@ -3,23 +3,30 @@ import { Favorite } from '../../models/Favorite';
 import { HomePage } from '../../pages/home/home';
 import { Category } from '../../models/Category';
 import { PhrasesPage } from '../../pages/phrases/phrases';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 
 
 @Component({
-  selector: 'favorites',
-  templateUrl: 'favorites.html'
+	selector: 'favorites',
+	templateUrl: 'favorites.html'
 })
 export class FavoritesComponent {
 	favorite: Favorite;
 
-	constructor(public navCtrl:NavController) {
-		this.favorite=HomePage.favClass;
+	private isIOS: boolean = false;
+
+	constructor(public navCtrl: NavController, public platform: Platform) {
+		this.favorite = HomePage.favClass;
+		//if this is iOS disable the sticky in the html
+		if (platform.is('ios'))
+			this.isIOS = true;
+		else
+			this.isIOS = false;
 	}
-	 //popup the category's phrases's page.
-	 public openCategoryPhrases(category: Category) {
-    this.navCtrl.push(PhrasesPage, {parentCategory: category});
-  }
+	//popup the category's phrases's page.
+	public openCategoryPhrases(category: Category) {
+		this.navCtrl.push(PhrasesPage, { parentCategory: category });
+	}
 
 
 }
